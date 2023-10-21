980bce6c5fa32687261e8fb3ef508a5656503d3e
Arno Vandijck <vandijckarno@hotmail.com>
2023-10-21 15:22:41 +0200

    my-todo-app added

---
+++ my-todo-app/src/test/java/com/arnovandijck/springboot/mytodoapp/MyTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnovandijck.springboot.mytodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
