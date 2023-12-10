f5657130deb7da20fe1cd4ce6f744f6f39467f05
Arno Vandijck <vandijckarno@hotmail.com>
2023-12-10 20:56:05 +0100

    Section 16 - Step 13 finished
    Cors and CSRF implemented + project change from gradle to maven

---
+++ learn-spring-security/src/main/java/com/arnovandijck/learnspringsecurity/basic/Roles.java
@@ -0,0 +1,6 @@
+package com.arnovandijck.learnspringsecurity.basic;
+
+public enum Roles {
+    USER,
+    ADMIN
+}
--- learn-spring-security/src/main/java/com/arnovandijck/learnspringsecurity/resources/TodoResource.java
+++ learn-spring-security/src/main/java/com/arnovandijck/learnspringsecurity/resources/TodoResource.java
@@ -1,14 +1,10 @@
 package com.arnovandijck.learnspringsecurity.resources;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TodoResource {
