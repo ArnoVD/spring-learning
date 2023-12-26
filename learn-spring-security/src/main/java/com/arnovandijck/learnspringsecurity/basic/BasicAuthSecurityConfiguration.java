c81dce620e050406ea4d516826950e43fb4608df
Arno Vandijck <vandijckarno@hotmail.com>
2023-12-26 12:21:50 +0100

    Section 16 - Spring security finished

--- learn-spring-security/src/main/java/com/arnovandijck/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/arnovandijck/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,6 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import javax.sql.DataSource;
 
 @Configuration
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 public class BasicAuthSecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -43,13 +46,17 @@ public class BasicAuthSecurityConfiguration {
     public UserDetailsService userDetailsService(DataSource dataSource) {
         // Create a user with username arno and password root
         var user = User.withUsername("arno")
-                .password("{noop}root")
+                //.password("{noop}root")
+                .password("root")
+                .passwordEncoder(passwordEncoder()::encode)
                 .roles(Roles.USER.name())
                 .build();
 
         // Create a user with username admin and password root
         var admin = User.withUsername("admin")
-                .password("{noop}root")
+                //.password("{noop}root")
+                .password("root")
+                .passwordEncoder(passwordEncoder()::encode)
                 .roles(Roles.USER.name(), Roles.ADMIN.name())
                 .build();
 
@@ -59,6 +66,11 @@ public class BasicAuthSecurityConfiguration {
         jdbcUserDetailsManager.createUser(admin);
 
         return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
--- learn-spring-security/src/main/java/com/arnovandijck/learnspringsecurity/resources/TodoResource.java
+++ learn-spring-security/src/main/java/com/arnovandijck/learnspringsecurity/resources/TodoResource.java
@@ -1,7 +1,11 @@
 package com.arnovandijck.learnspringsecurity.resources;
 
+import jakarta.annotation.security.RolesAllowed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,9 +25,14 @@ public class TodoResource {
     }
 
     @GetMapping("/users/{username}/todos")
+    // Pre-and Post authorization are the recommended way to do authorization
+    @PreAuthorize("hasRole('USER') and #username == authentication.name") // User should have role ROLE_USER and username should be the same as the authenticated user
+    @PostAuthorize("returnObject.username == 'in28minutes'") // The username of the returned object should be the same as the authenticated user
+    @RolesAllowed({"ADMIN", "USER"}) // User should have role USER or ADMIN
+    @Secured({"ROLE_ADMIN", "ROLE_USER"}) // User should have role ROLE_USER or ROLE_ADMIN
     public Todo retrieveTodosForSpecificUser(@PathVariable String username) {
         return TODOS_LIST.get(0);
-    }
+    }t
 
     @PostMapping("/users/{username}/todos")
     public void createTodoForSpecificUser(@PathVariable String username
